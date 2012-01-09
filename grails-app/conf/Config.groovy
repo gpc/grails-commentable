ca0fdfdd61b3a91a3f03a1e383e82a3ca56b663c
Peter Ledbrook <peter@cacoethes.co.uk>
2012-01-09 11:25:47 +0000

    Upgrade to Grails 2.0.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,1 +1,2 @@
-grails.commentable.poster.evaluator = { [id:1, "class":[name:"org.grails.comments.TestPoster"]] }
+grails.commentable.poster.evaluator = { [id:1, "class":[name:"org.grails.comments.TestPoster"]] }grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
